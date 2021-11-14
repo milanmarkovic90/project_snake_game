07b8f3e7e0bc7789a41bbcf1fe46f59ad60dd105
Milan Markovic <72462821+Relik9019@users.noreply.github.com>
2021-11-14 21:56:13 +0100

    Testing Directions.

--- src/snake_test.py
+++ src/snake_test.py
@@ -12,5 +12,23 @@ class TestSnake(unittest.TestCase):
         self.snake = Snake(self.surface, 1)
         self.assertEqual(self.snake.direction, 'down')
 
+    def test_moving_up_changes_direction_to_up(self):
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface, 1)
+        self.snake.move_up()
+        self.assertEqual(self.snake.direction, 'up')
+
+    def test_moving_left_changes_direction_to_left(self):
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface, 1)
+        self.snake.move_left()
+        self.assertEqual(self.snake.direction, 'left')
+
+    def test_moving_right_changes_direction_to_right(self):
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface, 1)
+        self.snake.move_right()
+        self.assertEqual(self.snake.direction, 'right')
+
 if __name__ == '__main__':
     unittest.main()
