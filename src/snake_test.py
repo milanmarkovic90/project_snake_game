a9a9d77a55cc2f28e8c774999e67d4335b67d126
Milan Markovic <milan.markovic9019@gmail.com>
2021-12-05 12:34:48 +0100

    Modified Pytest.

--- src/snake_test.py
+++ src/snake_test.py
@@ -10,11 +10,19 @@ class TestSnake(unittest.TestCase):
     def test_initial_direction_is_down(self):
         self.surface = pygame.display.set_mode((1000, 800))
         self.snake = Snake(self.surface, 1)
-        self.assertEqual(self.snake.direction, 'up')
+        self.assertEqual(self.snake.direction, 'down')
 
     def test_moving_up_changes_direction_to_up(self):
         self.surface = pygame.display.set_mode((1000, 800))
         self.snake = Snake(self.surface, 1)
+        self.snake.move_right()
+        self.snake.move_up()
+        self.assertEqual(self.snake.direction, 'up')
+
+    def test_moving_up_when_direction_is_down_then_keep_moving_down(self):
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface, 1)
+        self.snake.move_down()
         self.snake.move_up()
         self.assertEqual(self.snake.direction, 'down')
 
